b19e7d480121bfd6354e24dc0a57f0b5117ef1bc
turner-prize <hiphoperation@hotmail.com>
2018-10-23 18:59:54 +0100

    added index.html to repo

--- run.py
+++ run.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def hello():
-    return 'Hello, World!'
+    if request.method == 'POST':
+      result = request.form
+      print (result)
+      print (result['emailAddress'])
+      return render_template("index.html")
+    else:
+        return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
