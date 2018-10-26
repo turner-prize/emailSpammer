f57d3fcc6749b941b6303b3cfad4cf061d8bc364
turner-prize <hiphoperation@hotmail.com>
2018-10-26 09:17:20 +0100

    latest commit

--- mail.py
+++ mail.py
@@ -2,31 +2,31 @@ import smtplib
 from email.message import EmailMessage
 import os
 
-def SendEmail(InvoiceFolder):
+def SendEmail():
     smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
     smtpObj.ehlo()
     smtpObj.starttls()
-    smtpObj.login('email', 'PASSWORD')
+    smtpObj.login('test6974@outlook.com', '5d}]VcPz88o(BB=f~u.<')
     MyInvoicePath = InvoiceFolder
     MyInvoice = os.listdir(MyInvoicePath)
 
-    for i in MyInvoice:
-        AttachmentFile = os.path.join(MyInvoicePath,i)
-        Subject = i.split(" Invoice")[0]
-        fName = i
+    #for i in :
+        #AttachmentFile = os.path.join(MyInvoicePath,i)
+        Subject = "test subject"#i.split(" Invoice")[0]
+        #fName = i
         msg = EmailMessage()
         msg['Subject'] = Subject
         msg.set_content("This is an automated email.")
-        msg['From'] = ''
-        msg['To'] = ''
-        with open(AttachmentFile, 'rb') as content_file:
-            content = content_file.read()
-            msg.add_attachment(content, maintype='application/pdf', subtype='pdf', filename=fName)
+        msg['From'] = 'test6974@outlook.com'
+        msg['To'] = 'dan@masonbreese.com'
+        #with open(AttachmentFile, 'rb') as content_file:
+        #    content = content_file.read()
+        #    msg.add_attachment(content, maintype='application/pdf', subtype='pdf', filename=fName)
 
         smtpObj.send_message(msg)
 
     smtpObj.quit()
 
-SendEmail(r"C:\Users\Dan\Desktop\TestInvoices")
+SendEmail()
 
 
--- run.py
+++ run.py
@@ -7,14 +7,13 @@ import attachments
 app = Flask(__name__)
 
 
-EmailLogin = 'EMAIL' 
-EmailPassword = 'PASSWORD'
+EmailLogin = 'test6974@outlook.com' 
+EmailPassword = '5d}]VcPz88o(BB=f~u.<'
 
 @app.route('/', methods = ['POST', 'GET'])
 def hello():
     if request.method == 'POST':
       result = request.form
-      print(result)
       SetAttributes(result)
       return render_template("index.html")
     else:
@@ -22,27 +21,36 @@ def hello():
 
 
 def SetAttributes(result):
-    SendTo = result['emailAddress']
-    NumberOfEmails = int(result['totalToSend'])
-    subject= result.get('subjectline','Subject')
     if result.get('attachments'):
-        print("att")
-        attachments.Generate(result['extension'],result.get('minFileSize',1),result.get('maxFileSize',10),int(result['attachmentsPerEmail']))
-    #SendEmail(SendTo,subject,NumberOfEmails)
+        attachments.Generate(result['extension'],
+                            result.get('minFileSize',1),
+                            result.get('maxFileSize',10),
+                            int(result['attachmentsPerEmail'])*int(int(result['totalToSend'])))
+    SendEmail(result)
     attachments.Remove()
 
-def SendEmail(to,subject,amount):
+def SendEmail(result):
     smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
     smtpObj.ehlo()
     smtpObj.starttls()
-    smtpObj.login(EmailLogin, EmailPassword)
+    smtpObj.login('test6974@outlook.com', '5d}]VcPz88o(BB=f~u.<')
 
-    for i in range(amount):
+    for i in range(int(result['totalToSend'])):
         msg = EmailMessage()
-        msg['Subject'] = subject + str(i)
+        Subject = f"{result.get('subjectline','Subject')} {i}"
+        msg['Subject'] = Subject
         msg.set_content("This is an automated email.")
-        msg['From'] = EmailLogin
-        msg['To'] = to
+        msg['From'] = 'test6974@outlook.com'
+        msg['To'] = result['emailAddress']
+        if result.get('attachments'):
+            for j in range(1,int(result['attachmentsPerEmail'])):
+                AttachmentFile = os.listdir('temp')
+                MyAttachment = os.path.join('temp',AttachmentFile[0])
+                with open(MyAttachment, 'rb') as content_file:
+                    content = content_file.read()
+                    msg.add_attachment(content, maintype='application/pdf', subtype='pdf', filename=AttachmentFile[0])
+                    os.remove(MyAttachment)
+
         smtpObj.send_message(msg)
 
     smtpObj.quit()
